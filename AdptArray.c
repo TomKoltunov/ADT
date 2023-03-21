c2ce2921b662b6192c8809c12df09aa16d08b78a
TomKoltunov <74180693+TomKoltunov@users.noreply.github.com>
2023-03-21 21:43:33 +0200

    Comments been added

--- AdptArray.c
+++ AdptArray.c
@@ -1,10 +1,9 @@
 // I have used the solution for the first test in the previous semester in the course "Software Systems A". Actually the whole core
 // functionality was already satisfied there so the most of what I did was to add and adapt to the current task. The core functionality 
-// has been given in the next link: "https://moodlearn.ariel.ac.il/course/view.php?id=101828" (there the solution to the relevant exam)
-// is under the name 'moed-a-sol'.
+// has been given in the next link: "https://moodlearn.ariel.ac.il/course/view.php?id=101828" (there is the solution to the relevant 
+// exam is under the name 'moed-a-sol'). This solution includes some of the comments that I hadn'd removed (lines 44, 56, 60).
 
 #include "AdptArray.h"
-#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,8 +15,9 @@ typedef struct AdptArray_
 	DEL_FUNC delFunc;
 	COPY_FUNC copyFunc;
     PRINT_FUNC printFunc;
-}AdptArray, *PAdptArray;
+} AdptArray;
 
+// The function named "CreateAdptArray" initializes an empty array (it means without any elements).
 PAdptArray CreateAdptArray(COPY_FUNC copyFunc_, DEL_FUNC delFunc_, PRINT_FUNC printFunc)
 {
 	PAdptArray pArr = (PAdptArray)malloc(sizeof(AdptArray));
@@ -31,6 +31,8 @@ PAdptArray CreateAdptArray(COPY_FUNC copyFunc_, DEL_FUNC delFunc_, PRINT_FUNC pr
 	return pArr;
 }
 
+// The function named "SetAdptArrayAt" receives index and element and keeps a copy of the elements in the desired location.
+// It frees the "old" element if exists.
 Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem)
 {
 	PElement* newpElemArr;
@@ -39,6 +41,7 @@ Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem)
 
 	if (idx >= pArr->ArrSize)
 	{
+        // Extend Array
 		if ((newpElemArr = (PElement*)calloc((idx + 1), sizeof(PElement))) == NULL)
 			return FAIL;
 		memcpy(newpElemArr, pArr->pElemArr, (pArr->ArrSize) * sizeof(PElement));
@@ -50,13 +53,16 @@ Result SetAdptArrayAt(PAdptArray pArr, int idx, PElement pNewElem)
 	}
     if(pArr->pElemArr[idx] != NULL) // Without this check we'll receive a Segmentation fault (core dumped)
     {
+        // Delete Previous Elem
         pArr->delFunc((pArr->pElemArr)[idx]);
     }
 	(pArr->pElemArr)[idx] = pArr->copyFunc(pNewElem);
+    // Update Array Size
 	pArr->ArrSize = (idx >= pArr->ArrSize) ? (idx + 1) : pArr->ArrSize;
 	return SUCCESS;
 }
 
+// The function named "DeleteAdptArray" frees the memory of an object (including his elements)
 void DeleteAdptArray(PAdptArray pArr)
 {
 	int i;
@@ -73,6 +79,7 @@ void DeleteAdptArray(PAdptArray pArr)
 	free(pArr);
 }
 
+// The function named "GetAdptArrayAt" receives index and returns a copy of the element in this location.
 PElement GetAdptArrayAt(PAdptArray pArr, int idx) 
 {
     if (pArr->pElemArr[idx] == NULL || idx >= pArr->ArrSize)
@@ -82,11 +89,13 @@ PElement GetAdptArrayAt(PAdptArray pArr, int idx)
     return pArr->copyFunc((pArr->pElemArr)[idx]);
 }
 
+// The function named "GetAdptArraySize" returns the size of the array (-1 if the array wasn't initialized successfully).
 int GetAdptArraySize(PAdptArray pArr)
 {
     return pArr->ArrSize;
 }
 
+// The function named "PrintDB" prints the elements in the array.
 void PrintDB(PAdptArray pArr)
 {
     int i;
@@ -94,7 +103,7 @@ void PrintDB(PAdptArray pArr)
 		return;
 	for(i = 0; i < pArr->ArrSize; ++i)
 	{
-		if (pArr->pElemArr[i] != NULL)
+		if (pArr->pElemArr[i] != NULL) // Without this check we'll receive a Segmentation fault (core dumped)
         {
             pArr->printFunc(pArr->pElemArr[i]);
         }
